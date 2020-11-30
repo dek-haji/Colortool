004889855df4c83b4fd69dc5e93af4e1ab9e2693
dek-haji <deeeqhaji@gmail.com>
2020-11-29 19:39:39 -0600

    added new RGB color picker

--- index.js
+++ index.js
@@ -16,9 +16,24 @@ hexInput.addEventListener('keyup', () => {
     inputColor.style.backgroundColor = "#" + strippedHex;
 })
 //Create a function to convert Hex to RGB
-const convertHexToRGB = () => {
-    if (!hex) return false;
+const convertHexToRGB = (hex) => {
+    if (!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace('#', '');
+
+    if (strippedHex.length === 3) {
+        strippedHex = strippedHex[0] + strippedHex[0] +
+            strippedHex[1] + strippedHex[1] +
+            strippedHex[2] + strippedHex[2];
+    }
+   const r  = parseInt(strippedHex.substring(0,2), 16);
+  const g  = parseInt(strippedHex.substring(2,4), 16);
+  const b   = parseInt(strippedHex.substring(4,6), 16);
+  
+  return {r,g,b}
 }
+
+console.log(convertHexToRGB("fff"));
 //this should work with 3 or 6 character hex values
 //Hint - useParseInt(16) to convert a hex value to a decimal value
 //should return an object with 3 properties - r,g, and b
